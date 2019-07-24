4853e257626b5a4c3e43641bc7c0ef49f246343f
Edilson Silva <edilson_js@yahoo.com.br>
2019-07-24 09:56:37 -0300

    Criando os objetos Cliente, Contato e Endereco para teste

--- Clientes/Program.cs
+++ Clientes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NetOffice.ExcelApi;
+using Clientes.classes;
 
 
 namespace Clientes
@@ -8,7 +9,30 @@ namespace Clientes
     {
         static void Main(string[] args)
         {
-           
+           //Vamos instanciar a classe cliente para
+           //realizar o cadastro dos clientes
+            Cliente cli = new Cliente();
+            
+            //Vamos instanciar a classe contato
+            Contato ct = new Contato();
+            ct.telefone = "11-5568-9965";
+            ct.celular = "11-99658-9945";
+            ct.email = "helena@terra.com.br";
+
+            //Vamos instanciar a classe endereço
+            Endereco end = new Endereco();
+            end.logradouro = "Rua Nova";
+            end.numero = "170A";
+            end.complemento = "Casa 12";
+            end.bairro = "Jardim Legal";
+
+            cli.nome="Helena";
+            cli.idade=24;
+            cli.dataNascimento = DateTime.Parse("15/11/1995");
+            cli.contato = ct;
+            cli.endereco = end;
+
+            Console.WriteLine(cli.cadastrar());            
 
         }
     }
