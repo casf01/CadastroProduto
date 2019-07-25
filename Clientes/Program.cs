9a5ea94acf24cc006165d5f24014f6f9610b24b2
Caroline Souza <ticah.senac@gmail.com>
2019-07-25 12:09:13 -0300

    Projeto Cadastrando Cliente

--- Clientes/Program.cs
+++ Clientes/Program.cs
@@ -16,23 +16,35 @@ namespace Clientes
             //Vamos instanciar a classe contato
             Contato ct = new Contato();
             ct.telefone = "11-5568-9965";
-            ct.celular = "11-99658-9945";
-            ct.email = "helena@terra.com.br";
+            ct.celular = "11-44558-9945";
+            ct.email = "pedro@terra.com.br";
 
             //Vamos instanciar a classe endereço
             Endereco end = new Endereco();
-            end.logradouro = "Rua Nova";
-            end.numero = "170A";
-            end.complemento = "Casa 12";
-            end.bairro = "Jardim Legal";
+            end.logradouro = "Rua Esperança";
+            end.numero = "20";
+            end.complemento = "Casa dos fundos";
+            end.bairro = "Jardim Maravilha";
 
-            cli.nome="Helena";
-            cli.idade=24;
-            cli.dataNascimento = DateTime.Parse("15/11/1995");
+            cli.nome="Jessica Fernandes";
+            cli.idade=23;
+            cli.dataNascimento = DateTime.Parse("15/11/1996");
             cli.contato = ct;
             cli.endereco = end;
 
-            Console.WriteLine(cli.cadastrar());            
+            //Console.WriteLine(cli.cadastrar());
+
+            //Passando todos os dados da matriz listar que está carregada com os dados dos clientes para um novo array.
+            //Assim esses dados serão carregados de uma só vez.
+
+             string[,] info = cli.listar();
+
+             for(int i = 0 ; i < 10; i++){
+                 for(int p = 0; p < 10; p++){
+                     Console.Write(info[i,p]+"\t");
+                 }
+                 Console.WriteLine();
+             }
 
         }
     }
--- Clientes/classes/Cliente.cs
+++ Clientes/classes/Cliente.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using NetOffice.ExcelApi;
 
 namespace Clientes.classes
@@ -11,33 +12,111 @@ namespace Clientes.classes
         public Contato contato;
         public Endereco endereco;
 
-        public string cadastrar(){
+        public string cadastrar()
+        {
 
-            string msg = "";
             Application ex = new Application();
-            ex.Visible = true;//Abrir o microsoft excel
-            ex.Workbooks.Add();//Adicionar uma planilha ao microsoft Excel
-            ex.Range("a1").Value = nome;
-            ex.Range("b1").Value = idade;
-            ex.Range("c1").Value = dataNascimento;
-            ex.Range("d1").Value = contato.telefone;
-            ex.Range("e1").Value = contato.celular;
-            ex.Range("f1").Value = contato.email;
-            ex.Range("g1").Value = endereco.logradouro;
-            ex.Range("h1").Value = endereco.numero;
-            ex.Range("i1").Value = endereco.complemento;
-            ex.Range("j1").Value = endereco.bairro;
-            //Vamos salvar o arquivo
-            ex.ActiveWorkbook.SaveAs(@"c:\edilson\cliente.xlsx");
+            FileInfo arquivo = new FileInfo(@"c:\Caroline\cliente.xlsx");
+            if (arquivo.Exists)
+            {
+                ex.Visible = true;//Abrir o microsoft excel
+                //Vamos abrir o arquivo existente
+                ex.Workbooks.Open(@"c:\Caroline\cliente.xlsx");
+                /*
+                Vamos fazer um laço com for para percorrer as linhas do excel 
+                e encontrar uma linha vazia. Quando encontrar essa linha ele
+                deve parar e escrever a linha com os dados do cliente
+                 */
+                for (int x = 3; x <= 100; x++)
+                {
 
-            //fechar o arquivo de excel
-            ex.Quit();
+                    if (ex.Range("A" + x).Value == null)
+                    {
+                        ex.Range("a" + x).Value = nome;
+                        ex.Range("b" + x).Value = idade;
+                        ex.Range("c" + x).Value = dataNascimento;
+                        ex.Range("d" + x).Value = contato.telefone;
+                        ex.Range("e" + x).Value = contato.celular;
+                        ex.Range("f" + x).Value = contato.email;
+                        ex.Range("g" + x).Value = endereco.logradouro;
+                        ex.Range("h" + x).Value = endereco.numero;
+                        ex.Range("i" + x).Value = endereco.complemento;
+                        ex.Range("j" + x).Value = endereco.bairro;
+
+                        //Vamos parar o laço for.
+                        break;
+                    }
+                    
+                }
+
+                //Vamos salvar o arquivo de excel
+                ex.ActiveWorkbook.Save();
+                //Fechar o arquivo
+                ex.Quit();
+            }
+            else
+            {
+                ex.Visible = true;//abra o excel
+                ex.Workbooks.Add();//adicionar uma planilha em branco
+                                   /*
+                                   Vamos montar o cabeçalho das colunas na linha 1
+                                    */
+                ex.Range("a1").Value = "Nome do Cliente";
+                ex.Range("b1").Value = "Idade";
+                ex.Range("c1").Value = "Data de Nascimento";
+                ex.Range("d1").Value = "Tel. Residencial";
+                ex.Range("e1").Value = "Celular";
+                ex.Range("f1").Value = "E-Mail";
+                ex.Range("g1").Value = "Logradouro";
+                ex.Range("h1").Value = "Número";
+                ex.Range("i1").Value = "Complemento";
+                ex.Range("j1").Value = "Bairro";
+
+                /*
+                Vamos formatar aplicando negrito, tamanho de fonte diferente e letra diferente
+                 */
+                ex.Range("a1:j1").Font.Name = "Tahoma";
+                ex.Range("a1:j1").Font.Bold = true;
+                ex.Range("a1:j1").Font.Size = 15;
+
+                ex.Range("a2").Value = nome;
+                ex.Range("b2").Value = idade;
+                ex.Range("c2").Value = dataNascimento;
+                ex.Range("d2").Value = contato.telefone;
+                ex.Range("e2").Value = contato.celular;
+                ex.Range("f2").Value = contato.email;
+                ex.Range("g2").Value = endereco.logradouro;
+                ex.Range("h2").Value = endereco.numero;
+                ex.Range("i2").Value = endereco.complemento;
+                ex.Range("j2").Value = endereco.bairro;
+
+               
+                ex.ActiveWorkbook.SaveAs(@"c:\Caroline\cliente.xlsx");
+                ex.Quit();
+            }
+
+
 
             return "Cliente salvo com sucesso!";
 
         }
-        public string[,] listar(){
-            return null;
+        public string[,] listar()
+        {
+            //Vamos construir uma matriz de string para 
+            //guardar os dados dos clientes
+            string[,] dados = new string[10,10];
+
+            Application excel = new Application();
+            excel.Visible = true;
+            excel.Workbooks.Open(@"c:\Caroline\cliente.xlsx");
+            for(int lin=1; lin <= 10; lin++){
+                for(int col = 1; col <= 10; col++){
+                    dados[lin-1,col-1] = excel.Cells[lin,col].Text.ToString();
+                }
+            }
+            excel.Quit();
+
+            return dados;
         }
 
 
